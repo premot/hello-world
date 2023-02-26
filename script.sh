e73ca09d1f59c560a41dff8cd51906ccf61a705f
premote <premote@proton.me>
2023-02-26 18:35:02 +0200

    send

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 sleep $((RANDOM % 36))
 echo $(curl icanhazip.com) | tr . a >> ~/hello-world/fileNewest
 date >> ~/hello-world/fileNewest
-printf "\n"
   git add .
    git commit -m "send"
     git push 
